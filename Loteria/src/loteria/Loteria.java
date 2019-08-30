6c101aaa26104cfdc12c277f0efc78b6c1fe54b1
Usuario padrao <gervasio.wd@gmail.com>
2019-08-29 22:22:37 -0300

    Preenchimento do array

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -6,16 +6,37 @@ import java.util.Scanner;
 public class Loteria {
 
     public static void main(String[] args) {
+        
+        int[] valores = new int[] {0,0,0,0,0,0};
+        imprimeVetor(valores);
+        
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 números");
         for (int i = 0; i < 6; i++) {
-            capNum(in,i);
+            int num = capNum(in,i);
+            while (num == -1){
+                num = capNum(in, i);
+            }
         }
     }
     
     public static int capNum (Scanner in, int i){
         System.out.println("Digite o " + (i+1) + "º número");
-        return 0;
+        int num = in.nextInt();
+        
+        if (num>=1 && num<=60) {
+            return num;
+        } else {
+            return -1;
+        }
+        
+        
     }
     
+    public static void imprimeVetor(int[] vet){
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i]+" ");
+        }
+        System.out.println("");
+    }
 }
