490b8960caa5acf085866ae37d4048721a6a0039
Usuario padrao <none@gmail.com>
2019-09-05 20:03:27 -0300

    INICIO DO SORTEIO

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -1,49 +1,55 @@
-
 package loteria;
 
-import java.util.Scanner;
+// Gervásio
+import java.util.*;
 
 public class Loteria {
 
     public static void main(String[] args) {
-        
-        int[] valores = new int[] {0,0,0,0,0,0};
+
+        int[] valores = new int[]{0, 0, 0, 0, 0, 0};
         imprimeVetor(valores);
-        
+
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 números");
         for (int i = 0; i < 6; i++) {
-            int num = capNum(in,i);
-            while (num == -1){
+            int num = -1;
+            while (num == -1) {
                 num = capNum(in, i);
                 for (int j = 0; j < i; j++) {
-                    if (valores[j] == num){
+                    if (valores[j] == num) {
                         num = -1;
-                        break;
                     }
                 }
             }
             valores[i] = num;
         }
         imprimeVetor(valores);
+        Random r = new Random();
+
+        int[] bolas = new int[]{0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < 6; i++) {
+            int bola = (r.nextInt(60) + 1);
+            bolas[i] = bola;
+        }
+        imprimeVetor(bolas);
+
     }
-    
-    public static int capNum (Scanner in, int i){
-        System.out.println("Digite o " + (i+1) + "º número");
+
+    public static int capNum(Scanner in, int i) {
+        System.out.println("Digite o " + (i + 1) + "º número");
         int num = in.nextInt();
-        
-        if (num>=1 && num<=60) {
+
+        if (num >= 1 && num <= 60) {
             return num;
         } else {
             return -1;
         }
-        
-        
     }
-    
-    public static void imprimeVetor(int[] vet){
+
+    public static void imprimeVetor(int[] vet) {
         for (int i = 0; i < vet.length; i++) {
-            System.out.print(vet[i]+" ");
+            System.out.print(vet[i] + " ");
         }
         System.out.println("");
     }
